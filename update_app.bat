e09be1a3ee70bc69fb44c34ad1c6e3ea56be37e3
Travis <efcskatertravis@gmail.com>
2023-03-11 10:35:53 -0600

    Inital commit

---
+++ update_app.bat
@@ -0,0 +1,73 @@
+@ECHO off
+SET GamesDir=%cd%
+
+:APPID
+SET /P GID=Steam App ID:
+IF %GID%=="" (
+    ECHO ERROR: Please provide Steam App ID
+    GOTO APPID
+)
+
+ECHO.
+
+:FOLDER
+SET /P GLOC=Folder Name:
+IF %GLOC%=="" (
+    ECHO ERROR: Please provide game install directory
+    GOTO APPID
+)
+
+SET INSTLOC=%GamesDir%\games\%GLOC%\
+SET STEAMCMD=steamcmd.exe
+
+ECHO.
+ECHO Install Location: %INSTLOC%
+ECHO.
+
+IF NOT EXIST %INSTLOC% GOTO NOGAMEDIR
+
+:START
+IF NOT EXIST %STEAMCMD% GOTO NOSTEAM
+
+:INSTALL
+steamcmd +force_install_dir %INSTLOC% +login anonymous +app_update %GID% +validate +quit
+ECHO.
+ECHO SUCCESS: SteamCMD game installation has finished
+ECHO.
+GOTO EXIT
+    
+:NOGAMEDIR
+ECHO WARNING: Game install directory not found 
+ECHO.
+SET /P X=If new install continue, otherwise cancel (1-continue, 2-cancel):
+ECHO.
+IF %X%==1 GOTO START
+IF %X%==2 GOTO CANCELLED
+
+:NOSTEAM
+ECHO.
+ECHO ERROR: Cannot find SteamCMD executable
+ECHO.
+pause
+EXIT
+
+:CANCELLED
+ECHO.
+ECHO CANCELLED: SteamCMD game installation has been cancelled
+ECHO.
+GOTO EXIT
+
+:FAILED
+ECHO.
+ECHO FAILED: SteamCMD game installation has failed
+ECHO.
+GOTO EXIT
+
+:RESTART
+CLS
+GOTO APPID
+
+:EXIT
+SET /P Y=Install another? (1-yes, 2-no):
+IF %Y%==1 GOTO RESTART
+IF %Y%==2 EXIT
